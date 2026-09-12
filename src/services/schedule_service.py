4cf5ac39da1732ec0cbdca9a90be32c25af3fa83
wertiko <kovtyn.fer@gmail.com>
2026-09-12 22:40:55 +0700

    feat: add TTL-based cache invalidation for schedule data

--- src/services/schedule_service.py
+++ src/services/schedule_service.py
@@ -1,22 +1,37 @@
+from time import monotonic
+
 from loguru import logger
 
 from sibsutis_schedule import AsyncSibsutisClient, MonthSchedule
 
+DEFAULT_TTL_SECONDS = 30 * 60
+
 
 class ScheduleService:
-    def __init__(self, login: str, password: str, proxy_url: str | None = None) -> None:
+    def __init__(
+        self,
+        login: str,
+        password: str,
+        proxy_url: str | None = None,
+        cache_ttl: int = DEFAULT_TTL_SECONDS,
+    ) -> None:
         self._client = AsyncSibsutisClient(login, password, proxy_url=proxy_url)
-        self._cache: dict[tuple[str, int], MonthSchedule] = {}
+        self._cache: dict[tuple[str, int], tuple[MonthSchedule, float]] = {}
+        self._ttl = cache_ttl
 
     async def get_month(self, group: str, month: int) -> MonthSchedule:
         key = (group, month)
         if key in self._cache:
-            logger.debug("Cache hit for group={}, month={}", group, month)
-            return self._cache[key]
+            schedule, cached_at = self._cache[key]
+            if monotonic() - cached_at < self._ttl:
+                logger.debug("Cache hit for group={}, month={}", group, month)
+                return schedule
+            del self._cache[key]
+            logger.debug("Cache expired for group={}, month={}", group, month)
 
         logger.info("Fetching schedule for group={}, month={}", group, month)
         schedule = await self._client.get_schedule(group, month=month)
-        self._cache[key] = schedule
+        self._cache[key] = (schedule, monotonic())
         return schedule
 
     async def search_groups(self, query: str) -> list[dict[str, str]]:
